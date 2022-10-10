2c11a48f6bb494ace6aeea392c33aa83ce60451c
Martin Průša <73097610+Martin-Prusa@users.noreply.github.com>
2022-10-10 12:14:20 +0200

    shuffle

--- src/main/java/cz/martin/services/TeamService.java
+++ src/main/java/cz/martin/services/TeamService.java
@@ -3,18 +3,22 @@ package cz.martin.services;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @ApplicationScoped
 public class TeamService {
 
-    public List<List<String>> generateTeams(String[] players, int numberOfTeams) {
+    public List<List<String>> generateTeams(String[] p, int numberOfTeams) {
+        List<String> players = Arrays.asList(p);
+        Collections.shuffle(players);
         List<List<String>> list = new ArrayList<>();
-        int playersInTeam = players.length / numberOfTeams;
+        int playersInTeam = players.size() / numberOfTeams;
         for (int i = 0; i < numberOfTeams; i++) {
             list.add(new ArrayList<String>());
             for (int j = 0; j < playersInTeam; j++) {
-                list.get(i).add(players[i*playersInTeam + j].trim());
+                list.get(i).add(players.get(i*playersInTeam + j).trim());
             }
         }
         return list;
